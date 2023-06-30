1c0be52c07a76e4953c0acc7f2f0fea7f2a10a1f
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-07-01 00:34:53 +0530

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ button.addEventListener('click', () => {
     const hue = Math.floor(Math.random() * 361);
     const sat = Math.floor(Math.random() * 101);
     const lum = Math.floor(Math.random() * 101);
-    const color = `hsl(${hue},${sat}%,${lum}%)`;
+    const color = `hsl(${hue}, ${sat}%, ${lum}%)`;
     container.style.backgroundColor = color;
     textHsl.innerText = color
     if ((sat < 20 && lum < 30) || lum<20) {
