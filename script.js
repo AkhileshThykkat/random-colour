2301ad70735d41baef433331ff39960dfac28975
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-06-30 23:34:58 +0530

    Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button');
 const container = document.querySelector('.container');
-const text = document.querySelector('h1');
+const textHsl = document.querySelector('.hsl');
+const textRgb = document.querySelector('.rgb');
 button.addEventListener('click', () => {
     
     const hue = Math.floor(Math.random() * 361);
@@ -8,11 +9,13 @@ button.addEventListener('click', () => {
     const lum = Math.floor(Math.random() * 101);
     const color = `hsl(${hue},${sat}%,${lum}%)`;
     container.style.backgroundColor = color;
-    text.innerText = color
+    textHsl.innerText = color
     if ((sat < 20 && lum < 30) || lum<20) {
         container.style.color = 'white';
     } else {
         container.style.color = 'black';
     }
+    const colorRgb = container.style.backgroundColor;
+    textRgb.innerText = colorRgb;
 })
 
